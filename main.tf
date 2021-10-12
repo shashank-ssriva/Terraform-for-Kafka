6ff680f6ba03684da00613a86150a5ed495665b2
Shashank Srivastava <shashank.ssriv@gmail.com>
2021-10-12 12:49:32 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "ap-south-1"
+}
+
+resource "aws_instance" "kafka_zookeper" {
+  ami           = "ami-041d6256ed0f2061c"
+  instance_type = "t2.micro"
+  key_name      = "kafka_ssh_key"
+
+  tags = {
+    Name = "KafkaZookeeper"
+  }
+}
