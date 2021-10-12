9072501371851d96d84e0db899d168cea8b5a06b
Shashank Srivastava <shashank.ssriv@gmail.com>
2021-10-12 13:58:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,14 +40,14 @@ resource "aws_instance" "kafka_zookeper" {
   }
 }
 
-output "SSH Access Security Group ID" {
+output "ssh_security_group_id" {
   value = aws_security_group.ssh_access.id
 }
 
-output "Zookeeper EC2 instance public IP" {
+output "zookeeper_instance_public_ip" {
   value = aws_instance.kafka_zookeper.public_ip
 }
 
-output "Zookeeper EC2 instance login" {
+output "zookeeper_instance_login" {
   value = "ssh ec2-user@${aws_instance.kafka_zookeper.public_ip} -i kafka_ssh_key.pem"
 }
